c2a89aca4bb6bce852fbd29d1200a32de67778b6
VallabhaniSwathi <164006246+VallabhaniSwathi@users.noreply.github.com>
2024-04-02 17:54:03 +0530

    Update Divisibility.java

--- Divisibility.java
+++ Divisibility.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DivisibilityChecker {
+public class Divisibility {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
